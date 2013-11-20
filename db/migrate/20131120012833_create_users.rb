c113a9d1cf181ddfce1ea9751f7ee5472817b9ad
Amal Hussein <amalhussein08@gmail.com>
2013-11-19 20:32:36 -0500

    ran migration for users table with the github user info and local user info

--- db/migrate/20131120012833_create_users.rb
+++ db/migrate/20131120012833_create_users.rb
@@ -1,6 +1,38 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
+    	t.string :first_name
+    	t.string :last_name
+    	t.boolean :is_admin
+    	t.string :telephone
+    	t.text :address 
+    	t.string :email
+    	t.string :emergency_contact
+    	t.string :emergency_contact_phone
+    	t.string   :github_url
+	    t.string   :html_url
+	    t.string   :repos_url
+	    t.string   :gists_url
+	    t.string   :avatar_url
+	    t.integer  :public_repos
+	    t.integer  :followers
+	    t.integer  :following
+	    t.integer  :github_id
+	    t.string   :github_login
+	    t.integer  :gravatar_id
+	    t.string   :followers_url
+	    t.string   :following_url
+	    t.string   :starred_url
+	    t.string   :subscriptions_url
+	    t.string   :organizations_url
+	    t.string   :events_url
+	    t.string   :received_events_url
+	    t.string   :type
+	    t.string   :site_admin
+	    t.string   :public_gists
+	    t.datetime :created_github_profile
+	    t.datetime :last_updated_github_at
+	    t.timestamps
     end
   end
 end
