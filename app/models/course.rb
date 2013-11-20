77f2a99f03616c5562bb0c21f291c4292a0691ca
Abby Howell <abby.g.howell@gmail.com>
2013-11-19 23:28:34 -0500

    working on scopes

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,7 +3,7 @@ class Course < ActiveRecord::Base
 	has_many :users, :through => :course_memberships
 	has_many :students, -> {where :course_memberships.role => 'student'}, class_name: 'User' 
 	has_many :teachers, -> {where :course_memberships.role => 'teacher'}, class_name: 'User' 
-	
+
 	has_many :assignments
 	# scope :current, -> { where("'start_date' < ?", Date.today) && ("'end_date' > ?", Date.today)}
   scope :past, -> { where("'end_date' < ?", Date.today)}
@@ -15,5 +15,5 @@ class Course < ActiveRecord::Base
   def students
   	self.users.where(:course_memberships => {:role => "student"})
   end
-  
+
 end
