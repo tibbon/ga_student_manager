6a0c837aa5f63b58c70b23fcab7e5eabb3f5ed21
Abby Howell <abby.g.howell@gmail.com>
2013-11-19 20:10:09 -0500

    added current and past scope lambdas to course model

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,4 +2,7 @@ class Course < ActiveRecord::Base
 	has_many :course_memberships
 	has_many :users, through: :course_memberships
 	has_many :assignments
+	scope :current, -> { where("'start_date' < ?", Date.today) && ("'end_date' > ?", Date.today)}
+  scope :past, -> { where("'end_date' < ?", Date.today)}
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ActiveRecord::Base
 	has_many :course_memberships
 	has_many :courses, through: :course_memberships
 	has_many :assignments, through: :student_assignments
+
 end
