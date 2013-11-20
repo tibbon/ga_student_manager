89f6c7820e5673310ff0dc66783bbbea8734eff6
Abby Howell <abby.g.howell@gmail.com>
2013-11-19 23:18:54 -0500

    added methods in course model to return teachers and students

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,12 +1,19 @@
 class Course < ActiveRecord::Base
 	has_many :course_memberships, :dependent => :destroy
 	has_many :users, :through => :course_memberships
-
-
 	has_many :students, -> {where :course_memberships.role => 'student'}, class_name: 'User' 
 	has_many :teachers, -> {where :course_memberships.role => 'teacher'}, class_name: 'User' 
 	
 	has_many :assignments
 	# scope :current, -> { where("'start_date' < ?", Date.today) && ("'end_date' > ?", Date.today)}
   scope :past, -> { where("'end_date' < ?", Date.today)}
+
+  def teachers
+    self.users.where(:course_memberships => {:role => "teacher"})
+  end
+
+  def students
+  	self.users.where(:course_memberships => {:role => "student"})
+  end
+  
 end
