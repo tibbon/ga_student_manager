ab27b5b8416143c8b132273ffe07bd953d7ed7a7
Abby Howell <abby.g.howell@gmail.com>
2013-11-19 20:23:12 -0500

    working on methods in user model, waiting for database so i can test them.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,13 @@ class User < ActiveRecord::Base
 	has_many :course_memberships
 	has_many :courses, through: :course_memberships
 	has_many :assignments, through: :student_assignments
+  
+  def role
+  	CourseMembership.current.where(user_id: self.id).role
+  end
+
+  def current_course
+  	CourseMembership.current.where(user_id: self.id).course_id
+  end
 
 end
