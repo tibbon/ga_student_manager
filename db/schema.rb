7306e6aaeb21d54668a98da195f2571ac40cdfe4
Amal Hussein <amalhussein08@gmail.com>
2013-11-19 20:41:19 -0500

    ran migration for course membership

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131120013326) do
+ActiveRecord::Schema.define(version: 20131120013455) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "course_memberships", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "course_id"
+    t.string   "role"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "courses", force: true do |t|
     t.string   "name"
