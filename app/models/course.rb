aef9826f45d2ef4d61979db9787bb39cd8e446b4
Abby Howell <abby.g.howell@gmail.com>
2013-11-19 20:59:34 -0500

    working on scopes for students and teachers

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,7 +1,17 @@
 class Course < ActiveRecord::Base
-	has_many :course_memberships
-	has_many :students, through: :course_memberships, :source => :user, :conditions => {:course_memberships => {:role => :student}}
-	has_many :teachers, through: :course_memberships, :source => :user, :conditions => {:course_memberships => {:role => :teacher}}
+	has_many :course_memberships, :dependent => :destroy
+	has_many :users, :through => :course_memberships, :uniq => true
+	has_many :students, through: :course_memberships, :source => :student, :conditions => "course_memberships.role = 'student'" do
+		def <<(student)
+			CourseMembership.with_scope(:create => {:role => "student"}) {self.concat student}
+		end
+	end
+	has_many :teachers, through: :course_memberships, :source => :teacher, :conditions => "course_memberships.role = 'teacher'" do
+		def <<(teacher)
+			CourseMembership.with_scope(:create => {:role => "teacher"}) {self.concat teacher}
+		end
+	end
+	
 	has_many :assignments
 	scope :current, -> { where("'start_date' < ?", Date.today) && ("'end_date' > ?", Date.today)}
   scope :past, -> { where("'end_date' < ?", Date.today)}
--- app/models/course_membership.rb
+++ app/models/course_membership.rb
@@ -1,4 +1,6 @@
 class CourseMembership < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :course
+	belongs_to :student, :class_name => "User"
+	belongs_to :teacher, :class_name => "User"
 end
