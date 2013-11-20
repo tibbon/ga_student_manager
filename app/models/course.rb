50f2680537d73a5bbdc8b4d4c48ef15af54955a1
Abby Howell <abby.g.howell@gmail.com>
2013-11-19 20:46:17 -0500

    editing role scopes

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,7 +1,7 @@
 class Course < ActiveRecord::Base
 	has_many :course_memberships
-	has_many :students, through: :course_memberships, :source => :user, :class_name => "User", :conditions => ['course_membership.role = ?', 'student']
-	has_many :teachers, through: :course_memberships, :source => :user, :class_name => "User", :conditions => ['course_membership.role = ?', 'teacher']
+	has_many :students, through: :course_memberships, :source => :user, :conditions => {:course_memberships => {:role => :student}}
+	has_many :teachers, through: :course_memberships, :source => :user, :conditions => {:course_memberships => {:role => :teacher}}
 	has_many :assignments
 	scope :current, -> { where("'start_date' < ?", Date.today) && ("'end_date' > ?", Date.today)}
   scope :past, -> { where("'end_date' < ?", Date.today)}
