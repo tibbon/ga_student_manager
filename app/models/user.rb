5ad864ec8d8bda89e8ffe8b89d68517e98fd58a8
Abby Howell <abby.g.howell@gmail.com>
2013-11-19 20:50:38 -0500

    added has_many student_assignments

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
 	has_many :course_memberships
 	has_many :courses, through: :course_memberships
+	has_many :student_assignments
 	has_many :assignments, through: :student_assignments
 
 	scope :with_role, lamda{|role| includes(:course_membership).where(:course_memberships => {:role => role}) }
