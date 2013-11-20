f3c8e7bd8eff16a1d5c99bc8e55af9904aa43b28
Rahul Horé <hore.rahul@gmail.com>
2013-11-19 21:17:44 -0500

    Tests Assignment model associations with shoulda

--- spec/models/assignment_spec.rb
+++ spec/models/assignment_spec.rb
@@ -1,4 +1,7 @@
 require 'spec_helper'
 
 describe Assignment do
+  it { should belong_to :course }
+  it { should have_many :student_assignments }
+  it { should have_many(:users).through :student_assignments }
 end
