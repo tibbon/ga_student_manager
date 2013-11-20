895ef7f59b9b6de1673475c5c652def650fbd546
Rahul Horé <hore.rahul@gmail.com>
2013-11-19 21:15:04 -0500

    Tests Course model associations with shoulda

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -1,4 +1,7 @@
 require 'spec_helper'
 
 describe Course do
+  it { should have_many :course_memberships }
+  it { should have_many(:users).through :course_memberships }
+  it { should have_many :assignments }
 end
