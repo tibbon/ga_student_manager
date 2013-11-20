c198a544cd98d11c339ba126e857cb9d7fc91e04
Abby Howell <abby.g.howell@gmail.com>
2013-11-19 20:33:46 -0500

    added role lambda, not sure if it will work until database and seeds are up and running

--- app/models/course.rb
+++ app/models/course.rb
@@ -4,5 +4,4 @@ class Course < ActiveRecord::Base
 	has_many :assignments
 	scope :current, -> { where("'start_date' < ?", Date.today) && ("'end_date' > ?", Date.today)}
   scope :past, -> { where("'end_date' < ?", Date.today)}
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,17 @@ class User < ActiveRecord::Base
 	has_many :course_memberships
 	has_many :courses, through: :course_memberships
 	has_many :assignments, through: :student_assignments
+
+	scope :with_role, lamda{|role| includes(:course_membership).where(:course_memberships => {:role => role}) }
+   # User.with_role "Student" 
   
   def role
   	CourseMembership.current.where(user_id: self.id).role
+  	#self.course_membership.current.role
   end
 
   def current_course
-  	CourseMembership.current.where(user_id: self.id).course_id
+  	CourseMembership.current.where(user_id: self.id).course
   end
 
 end
