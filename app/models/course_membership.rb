48fa989a44955450c9434d035f22acfce79207b9
Matt Clement <darkmoves@gmail.com>
2013-11-21 15:53:28 -0500

    Cleaning up comments Abby was using for reference.

--- app/models/course_membership.rb
+++ app/models/course_membership.rb
@@ -13,6 +13,4 @@
 class CourseMembership < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :course
-	# belongs_to :student, :class_name => "User"
-	# belongs_to :teacher, :class_name => "User"
 end
