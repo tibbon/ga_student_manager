402c846ddad19cbeeec098b661bb81f95822a544
Matt Clement <darkmoves@gmail.com>
2013-11-21 20:59:16 -0500

    All is well with user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,4 @@
 StudentManagerApp::Application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: "omniauth_callbacks" }
 	root 'users#index'
-
-
-  #Routes for GitHub Oauth - Probably don't need the below things
-  # get '/users/callback' => 'users#callback'
-  # get '/users/callback' => 'omni_auth_callbacks#github'
-  # get '/sessions/:id/create' => 'sessions#create', as: 'create_user_session'
-  # get '/sessions/:id/destroy' => 'sessions#destroy', as: 'destroy_user_session'
-  # get '/users/create' => 'users#create', as: 'create_user'
-  # get '/users/:id' => 'users#show', as: 'user' #have basic method in place for this now, but we need to refine the view & method
 end
