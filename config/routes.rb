8350d000337f1a85ee688c1e65d1135ab5cee4e1
Matt Clement <darkmoves@gmail.com>
2013-11-21 19:26:52 -0500

    Created oauth migration for users

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,11 @@ StudentManagerApp::Application.routes.draw do
 
 
   #Routes for GitHub Oauth
-  get '/users/callback' => 'users#callback'
-  get '/sessions/:id/create' => 'sessions#create', as: 'create_user_session'
-  get '/sessions/:id/destroy' => 'sessions#destroy', as: 'destroy_user_session'
-  get '/users/create' => 'users#create', as: 'create_user'
-  get '/users/:id' => 'users#show', as: 'user' #have basic method in place for this now, but we need to refine the view & method
+  # get '/users/callback' => 'users#callback'
+  # get '/sessions/:id/create' => 'sessions#create', as: 'create_user_session'
+  # get '/sessions/:id/destroy' => 'sessions#destroy', as: 'destroy_user_session'
+  # get '/users/create' => 'users#create', as: 'create_user'
+  # get '/users/:id' => 'users#show', as: 'user' #have basic method in place for this now, but we need to refine the view & method
 end
 
 
