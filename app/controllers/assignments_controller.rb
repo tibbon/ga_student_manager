4ca4c1db8b66f5db284ceaa77519c1e922fd1f55
Amal Hussein <amalhussein08@gmail.com>
2013-11-24 10:44:44 -0500

    added methods to assignment controller, and params, and private methods as well

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -13,6 +13,20 @@ class AssignmentsController < ApplicationController
 	def create
 	end 
 
+	def edit
+	end 
 
+	def update
+	end 
+
+private 
+
+  def set_assignement
+  	@assignment = Assignment.find(params[:id])
+  end 
+
+  def assignment_params
+    params.require(:assignment).permit(:github_login, :course_id,:title, :github_repo, :due_date, :assignment_type, :description, :title) 
+  end 
 
 end
