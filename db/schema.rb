73d5a70636774ffb57d61b1724d57ec9972333c7
Amal Hussein <amalhussein08@gmail.com>
2013-11-24 17:06:16 -0500

    troubleshooting bootstrap

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131124212318) do
+ActiveRecord::Schema.define(version: 20131124212323) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,7 +39,7 @@ ActiveRecord::Schema.define(version: 20131124212318) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "github_id"
-
+    t.string   "travis_url"
   end
 
   add_index "contributions", ["assignment_id"], name: "index_contributions_on_assignment_id", using: :btree
