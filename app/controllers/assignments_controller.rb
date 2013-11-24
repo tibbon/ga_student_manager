4fffd6d5624af29cb23fa9d4c4ede96bcfbabcdb
Amal Hussein <amalhussein08@gmail.com>
2013-11-24 10:37:37 -0500

    working on assignments controller

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -1,8 +1,18 @@
 class AssignmentsController < ApplicationController
-	
+		
+  before_action :set_assignment, only: [:show, :edit, :update, :destroy]
+
 	def show 
 		@assignment = Assignment.find(params[:id])
 		@assignment.contributions
 	end 
+
+	def new
+	end 
+
+	def create
+	end 
+
+
 
 end
