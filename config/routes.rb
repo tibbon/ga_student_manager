dce8388c39835201a4e31d59cde21c8c2bda4731
David Fisher <tibbon@gmail.com>
2013-12-05 11:37:06 -0500

    Removed unused javascript files

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 StudentManagerApp::Application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: "omniauth_callbacks" }
-	root 'users#index'
-	get '/assignments/:id' => 'assignments#show'
-
+	root 'main#index'
 end
